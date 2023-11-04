9b7cf51de702413459d1677460dfb2621e661832
Shashank Suman <sumanshashank616@gmail.com>
2023-11-04 13:08:11 +0530

    Create HR_Q2_String(comp).java
    We define the following terms:
    
    Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
    For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
    
    A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
    Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
    
    Function Description
    
    Complete the getSmallestAndLargest function in the editor below.
    
    getSmallestAndLargest has the following parameters:
    
    string s: a string
    int k: the length of the substrings to find
    Returns
    
    string: the string ' + "\n" + ' where and are the two substrings

---
+++ HR_Q2_String(comp).java
@@ -0,0 +1,53 @@
+/* We define the following terms:
+
+Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
+For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+
+A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+
+Function Description
+
+Complete the getSmallestAndLargest function in the editor below.
+
+getSmallestAndLargest has the following parameters:
+
+string s: a string
+int k: the length of the substrings to find*/
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length '
+        smallest = s.substring(0,k);
+        largest = s.substring(0,k);
+        for(int i=0; i<=s.length()-k; i++ ){
+            String str = s.substring(i,k+i);
+            if (smallest.compareTo(str)>0){
+                smallest = str;
+            }
+            if(largest.compareTo(str)<0){
+                largest=str;
+            }
+        }
+        
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
